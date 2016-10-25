bf44636fec481ffa3b325f26ecea373b1def2737
cardiomoon <cardiomoon@gmail.com>
2016-10-25 12:06:03 +0900

    0.1.0
    kormaps2014

---
+++ R/areacode.R
@@ -0,0 +1,111 @@
+#'Korean administative area and code
+#'
+#'A dataset
+#'
+#'  @format A data.frame with 16 rows and 3 columns
+#'  \describe{
+#'     \item{code}{Korean administative area code}
+#'     \item{name}{Korean administative area, level 1}
+#'     \item{name2}{abbreviation of area}
+#'}
+"areacode"
+
+
+
+#'Korean administative map data(2014) level 1
+#'
+#'A dataset
+#'
+#'  @format A data.frame with 8831 rows and 11 columns
+#'  \describe{
+#'     \item{id}{Korean administative area code}
+#'     \item{long}{longitude}
+#'     \item{lat}{latitude}
+#'     \item{order}{An integer}
+#'     \item{hole}{A logical}
+#'     \item{piece}{Factor with 102 levels}
+#'     \item{group}{Factor with 186 levels}
+#'     \item{SP_ID}{shape id}
+#'     \item{SIDO_CD}{sido code}
+#'     \item{SIDO_NM}{sido name in euc-kr}
+#'     \item{BASE_YEAR}{2014}
+#'     \item{name}{name in utf-8}
+#'     \item{name1}{name in euc-kr}
+#'     \item{region}{Region}
+#'     \item{code}{code}
+#'}
+"kormap1"
+
+#'Korean administative map data(2014) level 2
+#'
+#'A dataset
+#'
+#'  @format A data.frame with 20397 rows and 11 columns
+#'  \describe{
+#'     \item{id}{Korean administative area code}
+#'     \item{long}{longitude}
+#'     \item{lat}{latitude}
+#'     \item{order}{An integer}
+#'     \item{hole}{A logical}
+#'     \item{piece}{Factor with 37 levels}
+#'     \item{group}{Factor with 401 levels}
+#'     \item{SP_ID}{shape id}
+#'     \item{base_year}{2014}
+#'     \item{sigungu_cd}{sigungu code}
+#'     \item{sigungu_name}{sigungu name in euc-kr}
+#'     \item{FID}{An integer}
+#'     \item{name}{name in utf-8}
+#'     \item{name1}{name in euc-kr}
+#'     \item{region}{Region}
+#'     \item{code}{code}
+#'}
+"kormap2"
+
+#'Korean administative map data(2014) level 3
+#'
+#'A dataset
+#'
+#'  @format A data.frame with 73474 rows and 11 columns
+#'  \describe{
+#'     \item{id}{Korean administative area code}
+#'     \item{long}{longitude}
+#'     \item{lat}{latitude}
+#'     \item{order}{An integer}
+#'     \item{hole}{A logical}
+#'     \item{piece}{Factor with 11 levels}
+#'     \item{group}{Factor with 3593 levels}
+#'     \item{SP_ID}{shape id}
+#'     \item{base_year}{2014}
+#'     \item{adm_dr_cd}{dong name code}
+#'     \item{adm_dr_nm}{dong name in euc-kr}
+#'     \item{FID}{An integer}
+#'     \item{name}{name in utf-8}
+#'     \item{name1}{name in euc-kr}
+#'     \item{region}{Region}
+#'     \item{code}{code}
+#'}
+"kormap3"
+
+
+#'Korean population census data(2015) level 1
+#'
+#' A dataset
+#'
+#'  @format A data.frame with 17 rows and 25 columns
+"korpop2015_1"
+
+
+#'Korean population census data(2015) level 2
+#'
+#' A dataset
+#'
+#'  @format A data.frame with 306 rows and 25 columns
+"korpop2015_2"
+
+#'Korean population census data(2015) level 3
+#'
+#' A dataset
+#'
+#'  @format A data.frame with 3492 rows and 25 columns
+"korpop2015_3"
+
